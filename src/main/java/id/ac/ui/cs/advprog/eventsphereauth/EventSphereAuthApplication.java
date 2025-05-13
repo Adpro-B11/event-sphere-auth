83ecc49b259b09672c79ac677f5ae9e6aec3ca53
adlynfakhreyz <adlynuniacc@gmail.com>
2025-05-14 01:30:08 +0700

    feat: init

---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphereauth/EventSphereAuthApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eventsphereauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSphereAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSphereAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eventsphereauth/EventSphereAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eventsphereauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSphereAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
