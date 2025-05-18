19fe75aaa6f5f19fde6032f520b892a4be92cc92
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2025-05-19 02:00:16 +0700

    [RED] Add implementation on UserRepository For FindByUsername, FindByEmail

--- src/main/java/id/ac/ui/cs/advprog/eventsphereauth/repository/InMemoryUserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphereauth/repository/InMemoryUserRepository.java
@@ -18,6 +18,20 @@ public class InMemoryUserRepository implements UserRepository {
     }
 
     @Override
+    public Optional<User> findByUsername(String username) {
+        return users.values().stream()
+                .filter(user -> user.getUsername().equals(username))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return users.values().stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst();
+    }
+
+    @Override
     public void save(User user) {
         users.put(user.getId(), user);
     }
--- src/main/java/id/ac/ui/cs/advprog/eventsphereauth/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphereauth/repository/UserRepository.java
@@ -6,5 +6,7 @@ import java.util.Optional;
 
 public interface UserRepository {
     Optional<User> findById(String id);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
     void save(User user);
 }
