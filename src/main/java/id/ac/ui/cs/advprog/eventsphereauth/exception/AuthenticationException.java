8930f7392a4b33393e36b5bc3439c6ae2e3377a6
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2025-05-19 23:06:38 +0700

    [GREEN] Remove all unusable files

--- src/main/java/id/ac/ui/cs/advprog/eventsphereauth/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.eventsphereauth.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/id/ac/ui/cs/advprog/eventsphereauth/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.eventsphereauth.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
