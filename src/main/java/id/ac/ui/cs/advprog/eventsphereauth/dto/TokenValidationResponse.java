18a6e0785beb6a15145c18cb13c06fd3ac9845e9
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2025-05-19 08:56:26 +0700

    [RED] Add TokenValidationResponse Implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphereauth/dto/TokenValidationResponse.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.eventsphereauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenValidationResponse {
+    private boolean valid;
+    private String username;
+    private String userId;
+}
