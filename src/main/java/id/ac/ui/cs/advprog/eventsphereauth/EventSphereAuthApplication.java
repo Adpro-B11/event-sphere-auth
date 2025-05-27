22bf86f20d03b80d6e4d508b7829eb0cb369fb02
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2025-05-27 10:16:13 +0700

    [CHORE] fix main app and its tests

--- src/main/java/id/ac/ui/cs/advprog/eventsphereauth/EventSphereAuthApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphereauth/EventSphereAuthApplication.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eventsphereauth;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,17 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventSphereAuthApplication {
 
     public static void main(String[] args) {
-        // ── Load .env ────────────────────────────────────────────────
-        Dotenv dotenv = Dotenv.load();
-
-        // 2) JWT
-        System.setProperty("jwt.secret-key", dotenv.get("JWT_SECRET_KEY"));
-        System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
-        // 3) Admin user
-        System.setProperty("app.admin.email",    dotenv.get("APP_ADMIN_EMAIL"));
-        System.setProperty("app.admin.password", dotenv.get("APP_ADMIN_PASSWORD"));
-        // ────────────────────────────────────────────────────────────────
-
         SpringApplication.run(EventSphereAuthApplication.class, args);
     }
+
 }
--- src/test/java/id/ac/ui/cs/advprog/eventsphereauth/EventSphereAuthApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eventsphereauth/EventSphereAuthApplicationTests.java
@@ -2,14 +2,10 @@ package id.ac.ui.cs.advprog.eventsphereauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import id.ac.ui.cs.advprog.eventsphereauth.security.JwtAuthenticationFilter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class EventSphereAuthApplicationTests {
-    @MockBean JwtAuthenticationFilter jwtAuthFilter;
-    @MockBean UserDetailsService userDetailsService;
+
     @Test
     void contextLoads() {
     }
